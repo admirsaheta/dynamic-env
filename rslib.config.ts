cb43216e8254aeaeac5b9862ffb25ded0c8c5402
admirsaheta <admirxsaheta@gmail.com>
2025-01-27 22:26:27 +0100

    configure(build):rspack

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig } from '@rslib/core';
+import { defineConfig } from "@rslib/core";
 
 export default defineConfig({
   lib: [
     {
-      format: 'esm',
-      syntax: 'es2021',
+      format: "esm",
+      syntax: "es2021",
       dts: true,
     },
     {
-      format: 'cjs',
-      syntax: 'es2021',
+      format: "cjs",
+      syntax: "es2021",
     },
   ],
+  source: {
+    entry: {
+      index: "src/index.ts",
+      cli: "src/cli.ts",
+      "inject.env": "src/inject.env.ts",
+      environment: "src/environment.ts",
+    },
+  },
+  output: {
+    cleanDistPath: true,
+    sourceMap: true,
+  },
 });
